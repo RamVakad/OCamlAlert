21beabd6bd0c80f05c7b9de095cb765e285dca6b
RamVakad <RamVakad@gmail.com>
2019-05-05 17:53:37 -0400

    Admin Module Complete

--- admin.ml
+++ admin.ml
@@ -2,21 +2,38 @@ open Unix
 open Printf
 open Thread
 
-let alert sock =
+let addLineBreak str = str ^ "\n"
+
+let alert sock pass msg =
     let outputStream = (Unix.out_channel_of_descr sock) in
     Printf.fprintf outputStream "iAmAdmin\n";
     Pervasives.flush outputStream;
-    Printf.fprintf outputStream "SuperSecretPassword\n";
+    Printf.fprintf outputStream "%s" (addLineBreak pass);
     Pervasives.flush outputStream;
-    Printf.fprintf outputStream "FLOOD ALERT STAY SAFE\n";
+    Printf.fprintf outputStream "%s" (addLineBreak msg);
     Pervasives.flush outputStream;
     ()
 
+let getHead ls =
+  match ls with
+  | [] -> ""
+  | a :: b -> a
+
+let removeHead ls =
+  match ls with
+  | [] -> []
+  | a :: b -> b
+
+let getAlert ls = String.concat " " ls
+
+
 let _ =
-  Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
+  let argList = Array.to_list Sys.argv in
+  let pass = getHead (removeHead argList) in
+  let msg = getAlert (removeHead (removeHead argList)) in
   let sock = Unix.socket PF_INET SOCK_STREAM 0 in
     Unix.connect sock (ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 8484));
     Printf.printf "Connected to 127.0.0.1:8484...\n";
-    alert sock;
+    alert sock pass msg;
     Printf.printf "Closing the connection.";
     Unix.close sock
